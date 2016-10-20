803876fada17ac56b024d2b0503635c487eabce4
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2016-10-20 20:38:24 +0200

    Updated test to match the new structure

--- examples/tests/basics.js
+++ examples/tests/basics.js
@@ -1,45 +1,48 @@
 /* globals define */
 
-define(["browsertests"], function (browsertests) {
+define(["browsertests", "jquery"], function (browsertests) {
 
   return function () {
 
     return browsertests.open("/control/logout")
-      .then(function (container) {
+      .then(function () {
         browsertests.checkpoint("Search");
         return browsertests.open("/");
       })
       .then(function (container) {
         container.find(".gk-header__search__input").val("Socken");
-        container.find('.or-cartentry .gk-button--primary')[0].click();
-        return browsertests.executeRetryingAndWaitForReady(function () {
+        return browsertests.execute(function () {
           container.find(".gk-header__search__button")[0].click();
-        });
+        }, {retry: true});
       })
+      .then(browsertests.waitForDOMContentLoaded)
       .then(function (container) {
         browsertests.checkpoint("ADS");
-        return browsertests.executeRetryingAndWaitForReady(function () {
+        return browsertests.execute(function () {
           container.find('.gk-article:first-child a')[0].click();
-        });
+        }, {retry: true});
       })
+      .then(browsertests.waitForDOMContentLoaded)
       .then(function (container) {
         browsertests.checkpoint("Add to Cart");
-        return browsertests.executeRetrying(function () { // Layer (no "ready")
+        return browsertests.execute(function () { // Layer (no "ready")
           container.find('.ev-product__orderbutton.gk-button--enabled')[0].click();
-        });
+        }, {retry: true});
       })
       .then(function (container) {
         browsertests.checkpoint("View Cart");
-        return browsertests.executeRetryingAndWaitForReady(function () {
+        return browsertests.execute(function () {
           container.find('.or-cartentry .gk-button--primary')[0].click();
-        });
+        }, {retry: true});
       })
+      .then(browsertests.waitForDOMContentLoaded)
       .then(function (container) {
         browsertests.checkpoint("Checkout");
-        return browsertests.executeRetryingAndWaitForReady(function () {
+        return browsertests.execute(function () {
           container.find('.or-page__cart .gk-button--next')[0].click();
-        });
-      });
+        }, {retry: true});
+      })
+      .then(browsertests.waitForDOMContentLoaded);
 
   };
 
